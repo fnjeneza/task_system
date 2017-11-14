450a1f15c7297b31b37e0b7f073ec8db67f71713
Francois Njeneza <njenezamed@gmail.com>
2017-11-14 18:22:46 +0100

    use n queues for n cores

--- task_system.hpp
+++ task_system.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <vector>
+#include <atomic>
 #include <thread>
+#include <vector>
 
 #include "notification_queue.hpp"
 
@@ -12,13 +13,16 @@ class task_system{
         {
             for(unsigned n = 0; n != m_count; ++n)
             {
-                m_threads.emplace_back([&]{ run(); });
+                m_threads.emplace_back([&, n] { run(n); });
             }
         }
 
         ~task_system()
         {
-            m_q.done();
+            for (auto &q : m_qs)
+            {
+                q.done();
+            }
             for(auto & t: m_threads)
             {
                 t.join();
@@ -28,25 +32,30 @@ class task_system{
         template<typename F>
         void async(F&& f)
         {
-            m_q.push(std::forward<F> (f));
+            auto i = m_index++;
+            m_qs[i % m_count].push(std::forward<F>(f));
         }
 
     private:
-        void run()
-        {
-            while(true)
-            {
-                std::function<void()> f;
-                // retrieve the function to be run
-                // if false means the queue is empty and it is done
-                // so, break the loop
-                if(!m_q.pop(f)) break;
-                f();
-            }
+      void run(unsigned i)
+      {
+          while (true)
+          {
+              std::function<void()> f;
+              // retrieve the function to be run
+              // if false means the queue is empty and it is done
+              // so, break the loop
+              if (!m_qs[i].pop(f))
+              {
+                  break;
+              }
+              f();
+          }
         }
 
     private:
         const unsigned m_count{std::thread::hardware_concurrency()};
         std::vector<std::thread> m_threads;
-        notification_queue m_q;
+        std::vector<notification_queue> m_qs{m_count};
+        std::atomic<unsigned>           m_index{0};
 };
